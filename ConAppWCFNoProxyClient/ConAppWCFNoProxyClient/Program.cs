6afe452b7659b54c2e8e34c1e8daf51dacd71d1d
chanmmn <33271651+chanmmn@users.noreply.github.com>
2020-08-02 14:02:51 +0800

    Check in WCFCallWithoutAddServicereference
    WCF call without Add Service Reference or Add Project Reference

---
+++ ConAppWCFNoProxyClient/ConAppWCFNoProxyClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppWCFNoProxyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            //Create EndPoint address  
+            EndpointAddress endpointAddress = new EndpointAddress("http://localhost:50960/Service1.svc");
+
+            //Pass Binding and EndPoint address to ChannelFactory  
+            ChannelFactory<IService1> factory = new ChannelFactory<IService1>(binding, endpointAddress);
+
+            //Now create the new channel as below  
+            IService1 channel = factory.CreateChannel();
+
+            //Call the service method on this channel as below  
+            string result = channel.GetData(1);
+        }
+    }
+}
---
+++ ConAppWCFNoProxyClient/ConAppWCFNoProxyClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConAppWCFNoProxyClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConAppWCFNoProxyClient")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6da5e6c0-641f-4443-bdc0-a11c7c3334dd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
